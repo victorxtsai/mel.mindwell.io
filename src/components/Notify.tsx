a43e359ef0bd957a98afa2a1af90bb7d2895474f
victorxtsai <71499306+victorxtsai@users.noreply.github.com>
2025-04-11 20:12:27 +0000

    valid

--- src/components/Notify.tsx
+++ src/components/Notify.tsx
@@ -52,7 +52,7 @@ export default function Notify() {
             <button
               type="submit"
               disabled={status === 'loading'}
-              className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              className="flex-none rounded-md bg-[#2F52E0] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[#1098F7] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               {status === 'loading' ? 'Sending...' : 'Notify me'}
             </button>
